1122c14af74639a25a74799ca28669dafa84d64a
metalisx <sa.oudenijhuis@gmail.com>
2013-07-07 05:10:27 -0700

    added a generic REST service

--- common/common-rest/src/main/java/org/metalisx/common/rest/service/AbstractGenericRestService.java
+++ common/common-rest/src/main/java/org/metalisx/common/rest/service/AbstractGenericRestService.java
@@ -1,6 +1,5 @@
 package org.metalisx.common.rest.service;
 
-import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -16,12 +15,10 @@ import javax.ws.rs.Produces;
 
 import org.metalisx.common.domain.dao.AbstractDao;
 import org.metalisx.common.domain.dto.ContextDto;
+import org.metalisx.common.domain.dto.OrderBy;
 import org.metalisx.common.domain.dto.PageDto;
 import org.metalisx.common.gson.RestGsonConverter;
-import org.metalisx.common.rest.dto.entity.EntitiesDto;
-import org.metalisx.common.rest.dto.entity.EntityDto;
 import org.metalisx.common.rest.dto.entity.EntityFieldDto;
-import org.metalisx.common.rest.dto.entity.EntityMetadataDto;
 import org.metalisx.common.rest.dto.entity.PageMetadataDto;
 import org.metalisx.common.rest.metadata.MetadataProvider;
 import org.slf4j.Logger;
@@ -29,6 +26,10 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Abstract generic rest service.
+ * 
+ * Unfortunately the setEntityClass method is required for providing the typed class, 
+ * because the REST implementation makes it impossible to retrieve the class from 
+ * the generic type.
  * 
  * Simple use case of this class:
  * <ul>
@@ -39,9 +40,11 @@ import org.slf4j.LoggerFactory;
  * <li>inject the correct entity manager with @PersistenceContext</li>
  * <li>create a method and annotate it with @PostConstruct</li>
  * <li>in this method call setEntityManager with the injected entity manager</li>
+ * <li>in this method call setEntityClass with the class it manages, this is the generic type</li>
  * <li>create a sub class for the javax.ws.rs.core.Application class</li>
  * <li>annotate it with @ApplicationPath("/rest") and replace rest if you need</li>
  * <ul>
+ * 
  */
 public abstract class AbstractGenericRestService<T> {
 
@@ -58,10 +61,21 @@ public abstract class AbstractGenericRestService<T> {
 	private AbstractDao abstractDao = new AbstractDao() {
 	};
 
-	@SuppressWarnings("unchecked")
 	public AbstractGenericRestService() {
-		logger.debug("Initalized generic rest service " + this.getClass().getName());
-		this.entityClass = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+		logger.debug("Initalized generic rest service " + this.getClass().getName());		
+		/*
+		This is not working, the setEntityClass needs to be called for now.
+		Kept the code to enable when REST implementations can handle this kind of class setup.
+		When activating add @SuppressWarnings("unchecked") above the constructor.
+		Type type = ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+		if (type instanceof Class) {
+			System.out.println("is class");
+			this.entityClass = (Class<T>) type;
+		} else if (type instanceof ParameterizedType) {
+			System.out.println("is parameterizedTYpe");
+			this.entityClass = (Class<T>) ((ParameterizedType) type).getRawType();
+		}
+		*/
 	}
 
 	@GET
@@ -84,36 +98,42 @@ public abstract class AbstractGenericRestService<T> {
 	@Path("/page")
 	@Consumes("application/json")
 	@Produces("application/json")
-	public PageDto<?> getPage(ContextDto contextDto)
+	public PageDto<T> getPage(ContextDto contextDto)
 	        throws ClassNotFoundException, InstantiationException, IllegalAccessException {
 		return abstractDao.findAll(entityClass, contextDto);
+	}
+
+	@POST
+	@Path("/{entityClass}/list")
+	@Consumes("application/json")
+	@Produces("application/json")
+	public List<T> getList(OrderBy orderBy)
+	        throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		return abstractDao.findAll(entityClass, orderBy, null);
 	}
 
 	@GET
 	@Path("/")
 	@Produces("application/json")
-	public EntitiesDto get() throws ClassNotFoundException,
+	public List<T> get() throws ClassNotFoundException,
 	        InstantiationException, IllegalAccessException {
-		List<?> list = abstractDao.findAll(entityClass);
-		return new EntitiesDto(list, new EntityMetadataDto(metadataProvider.getEntityMetadata(entityClass)));
+		return abstractDao.findAll(entityClass);
 	}
 
 	@GET
 	@Path("/new-entity")
 	@Produces("application/json")
-	public EntityDto getNewEntity() throws ClassNotFoundException,
+	public T getNewEntity() throws ClassNotFoundException,
 	        InstantiationException, IllegalAccessException {
-		Object entity = entityClass.newInstance();
-		return new EntityDto(entity, new EntityMetadataDto(metadataProvider.getNewEntityMetadata(entityClass)));
+		return entityClass.newInstance();
 	}
 
 	@GET
 	@Path("/{id}")
 	@Produces("application/json")
-	public EntityDto get(@PathParam("id") Long id)
+	public T get(@PathParam("id") Long id)
 	        throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-		return new EntityDto(abstractDao.findById(entityClass, id), new EntityMetadataDto(
-				metadataProvider.getEntityMetadata(entityClass)));
+		return abstractDao.findById(entityClass, id);
 	}
 
 	@DELETE
@@ -129,7 +149,7 @@ public abstract class AbstractGenericRestService<T> {
 	@Path("/")
 	@Consumes("application/json")
 	@Produces("application/json")
-	public Object put(String body) throws ClassNotFoundException,
+	public T put(String body) throws ClassNotFoundException,
 	        InstantiationException, IllegalAccessException {
 		return abstractDao.persist(entityClass, restGsonConverter.fromJson(body, entityClass));
 	}
@@ -138,13 +158,17 @@ public abstract class AbstractGenericRestService<T> {
 	@Path("/")
 	@Consumes("application/json")
 	@Produces("application/json")
-	public Object post(String body) throws ClassNotFoundException,
+	public T post(String body) throws ClassNotFoundException,
 	        InstantiationException, IllegalAccessException {
 		return abstractDao.persist(entityClass, restGsonConverter.fromJson(body, entityClass));
 	}
 
 	public void setEntityManager(EntityManager entityManager) {
 		abstractDao.setEntityManager(entityManager);
+	}
+
+	public void setEntityClass(Class<T> entityClass) {
+		this.entityClass = entityClass;
 	}
 
 }
