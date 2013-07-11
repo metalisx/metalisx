443f89b5525bef09c7242451d60f2641326f33f6
metalisx <sa.oudenijhuis@gmail.com>
2013-07-11 15:21:48 -0700

    Added handling bean validation to the rest exception provider.

--- common/common-rest/src/main/java/org/metalisx/common/rest/provider/RestExceptionProvider.java
+++ common/common-rest/src/main/java/org/metalisx/common/rest/provider/RestExceptionProvider.java
@@ -2,8 +2,12 @@ package org.metalisx.common.rest.provider;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Iterator;
+import java.util.Set;
 
 import javax.persistence.PersistenceException;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -16,20 +20,47 @@ public class RestExceptionProvider implements ExceptionMapper<Exception> {
 
 	@Override
 	public Response toResponse(Exception exception) {
-		MessagesDto messagesDto = new MessagesDto();
-		if (exception instanceof PersistenceException) {
-			messagesDto.addError(getMessage(exception));
+		MessagesDto messagesDto;
+		Throwable rootCause = getRootCause(exception);
+		if (rootCause instanceof ConstraintViolationException) {
+			messagesDto = constraintViolationsToMessagesDto((ConstraintViolationException) rootCause);
+		} else if (exception instanceof PersistenceException) {
+			messagesDto = persistenceExceptionToMessagesDto((PersistenceException) exception);
 		} else {
-			messagesDto.addError(getMessage(exception), getStackTrace(exception));
+			messagesDto = exceptionToMessagesDto(exception);
 		}
-		return Response.ok().type(MediaType.APPLICATION_JSON).entity(messagesDto).build();
+		return Response.ok().type(MediaType.APPLICATION_JSON)
+				.entity(messagesDto).build();
+	}
+
+	private MessagesDto constraintViolationsToMessagesDto(ConstraintViolationException constraintViolationException) {
+		MessagesDto messagesDto = new MessagesDto();
+		Set<ConstraintViolation<?>> violations = constraintViolationException.getConstraintViolations();
+		Iterator<ConstraintViolation<?>> iterator = violations.iterator();
+		while (iterator.hasNext()) {
+			ConstraintViolation<?> constraintViolation = iterator.next();
+			messagesDto.addError(constraintViolation.getMessage());
+		}
+		return messagesDto;
+	}
+
+	private MessagesDto persistenceExceptionToMessagesDto(PersistenceException persistenceException) {
+		MessagesDto messagesDto = new MessagesDto();
+		messagesDto.addError(getRootCause(persistenceException).getMessage());
+		return messagesDto;
 	}
 	
-	private String getMessage(Throwable throwable) {
+	private MessagesDto exceptionToMessagesDto(Exception exception) {
+		MessagesDto messagesDto = new MessagesDto();
+		messagesDto.addError(getRootCause(exception).getMessage(), getStackTrace(exception));
+		return messagesDto;
+	}
+	
+	private Throwable getRootCause(Throwable throwable) {
 		if (throwable.getCause() != null) {
-			return getMessage(throwable.getCause());
+			return throwable.getCause();
 		}
-		return throwable.getMessage();
+		return throwable;
 	}
 	
 	private String getStackTrace(Throwable throwable) {
@@ -38,5 +69,5 @@ public class RestExceptionProvider implements ExceptionMapper<Exception> {
 		throwable.printStackTrace(printWriter);
 		return stringWriter.toString();
 	}
-	
+
 }
