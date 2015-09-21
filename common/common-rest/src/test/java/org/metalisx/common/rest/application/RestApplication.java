8d93f537cc8038af5e0dc91e836bcf2073cb9d43
Stefan Oude Nijhuis <sa.oudenijhuis@gmail.com>
2015-09-21 21:47:52 +0200

    Changed ApplicationPath to remove duplicate Application error in Eclipse

--- common/common-rest/src/test/java/org/metalisx/common/rest/application/RestApplication.java
+++ common/common-rest/src/test/java/org/metalisx/common/rest/application/RestApplication.java
@@ -3,7 +3,7 @@ package org.metalisx.common.rest.application;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/rest")
+@ApplicationPath("/common-rest-test")
 public class RestApplication extends Application {
 
 }
