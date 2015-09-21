01905d9f370aea251865ed80823ee85c6cd213b7
Stefan Oude Nijhuis <sa.oudenijhuis@gmail.com>
2015-09-21 21:59:06 +0200

    Changed ApplicationPath to remove Eclipse validator error "Multiple JAX-RS Activators are defined for the project"

--- common/common-rest/src/test/java/org/metalisx/common/rest/application/RestApplication.java
+++ common/common-rest/src/test/java/org/metalisx/common/rest/application/RestApplication.java
@@ -3,7 +3,7 @@ package org.metalisx.common.rest.application;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/common-rest-test")
+@ApplicationPath("/rest-test")
 public class RestApplication extends Application {
 
 }
